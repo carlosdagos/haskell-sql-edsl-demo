8bb2532313cdd19c8f689d5116f7862c38570d27
Carlos D'Agostino <carlos.dagostino@gmail.com>
2016-04-07 18:24:06 +0200

    wip

--- code/simple/src/Main.hs
+++ code/simple/src/Main.hs
@@ -5,8 +5,4 @@ import Simple.Hashtag              (Hashtag)
 import Database.PostgreSQL.Simple
 
 main :: IO ()
-main = do
-    conn     <- connect defaultConnectInfo
-    [Only i] <- query_ conn "select id, "
-
-    putStrLn i
+main = undefined
