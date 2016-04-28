d1ae1029d27ca2e94feca97c4e1dca3332e97a3a
Carlos D'Agostino <carlos.dagostino@gmail.com>
2016-04-28 15:12:05 +0200

    add todo

--- code/opaleye/src/OpaleyeDemo/Todo.hs
+++ code/opaleye/src/OpaleyeDemo/Todo.hs
@@ -6,38 +6,66 @@
 --------------------------------------------------------------------------------
 -- | See TodoManual.hs to avoid using TemplateHaskell, though you should be
 -- | aware that this will make you write more boilerplate code
-module OpaleyeDemo.Todo
-      ( -- * Exports
-        todoTable
-      , todoQuery
-      ) where
+module OpaleyeDemo.Todo where
 
+import Control.Lens
+       ( makeLenses )
+import Data.Time.Calendar
+       ( Day )
 import Opaleye
-import Data.Profunctor.Product
+       ( Table(..), Query, Column, Nullable, PGInt4, PGText, PGDate
+       , queryTable, required, optional )
+import Data.Profunctor.Product.TH
+       ( makeAdaptorAndInstance )
 
-data Todo' i t d p = Todo' { _id :: i
-                           , _title :: t
-                           , _dueDate :: d
-                           , _prio :: p
-                           }
-type TodoId = Int
-type Prio   = Maybe Int
-type Todo   = Todo' TodoId String Day Prio
+--------------------------------------------------------------------------------
+-- | Todo Id
+data TodoId' a = TodoId { todoId :: a } deriving Show
+makeAdaptorAndInstance "pTodoId" ''TodoId'
 
-type TodoColumn = Todo' ( Column PGInt4, Column PGText
-                        , Column PGDate, Maybe (Column PGInt4) )
+type TodoId = TodoId' Int
+type TodoIdColumn = TodoId' (Column PGInt4)
+type TodoIdColumnMaybe = TodoId' (Maybe (Column PGInt4))
+type TodoIdColumnNullable = TodoId' (Column (Nullable PGInt4))
 
+--------------------------------------------------------------------------------
+-- | Priority indicator
+data Prio' a = Prio { unPrio :: a } deriving Show
+makeAdaptorAndInstance "pPrio" ''Prio'
 
-$(makeAdaptorAndInstance "pTodo" ''Todo')
+type Prio = Prio' Int
+type PrioColumn = Prio' (Column PGInt4)
+type PrioColumnMaybe = Prio' (Maybe (Column PGInt4))
+type PrioColumnNullable = Prio' (Column (Nullable PGInt4))
 
-todoTable
-  :: Table (Column PGInt4, Column PGText, Column PGDate, Maybe (Column PGInt4))
-           (Column PGInt4, Column PGText, Column PGDate, Column PGInt4)
-todoTable = Table "todos" (p4 ( required "id"
-                              , required "title"
-                              , required "due_date"
-                              , optional "prio" ))
+--------------------------------------------------------------------------------
+-- | Todo declaration
+data Todo' i t d p = Todo { _id :: i
+                          , _title :: t
+                          , _dueDate :: d
+                          , _prio :: p
+                          }
 
-todoQuery :: Query (Column PGInt4, Column PGText, Column PGDate, Column PGInt4)
+makeLenses ''Todo'
+makeAdaptorAndInstance "pTodo" ''Todo'
+
+--------------------------------------------------------------------------------
+-- | Todo Columns
+type TodoColumns
+  = Todo' TodoIdColumn (Column PGText) (Column PGDate) PrioColumn
+type TodoInsertColumns
+  = Todo' TodoIdColumnMaybe (Column PGText) (Column PGDate) PrioColumnMaybe
+type Todo
+  = Todo' TodoId String Day Prio
+
+todoTable :: Table TodoInsertColumns TodoColumns
+todoTable = Table "todos" $ pTodo Todo
+    { _id      = pTodoId . TodoId $ optional "id"
+    , _title   = required "title"
+    , _dueDate = required "due_date"
+    , _prio    = pPrio . Prio $ optional "prio"
+    }
+
+todoQuery :: Query TodoColumns
 todoQuery = queryTable todoTable
 
